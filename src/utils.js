c75dc70fe064cdcade8f1911972e47d9a1ee71fe
evry1lovesjay <evry1lovesjay@yahoo.com>
2023-05-31 13:29:29 +0200

    added prod & dev

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,18 @@
 import  axios from "axios";
 
+
+const API_URL = process.env.NODE_ENV === 'production'
+  ? process.env.REACT_APP_API_URL_PROD
+  : process.env.REACT_APP_API_URL_DEV;
+
+// export const axiosInstance = axios.create({
+//     baseURL: "https://todo-v3-server.onrender.com/api"
+// })
+
 export const axiosInstance = axios.create({
-    baseURL: "https://todo-v3-server.onrender.com/api"
+    baseURL: `${API_URL}/api`   
 })
+
+// export const axiosInstance = axios.create({
+//     baseURL: "http://localhost:6400/api"
+// })
